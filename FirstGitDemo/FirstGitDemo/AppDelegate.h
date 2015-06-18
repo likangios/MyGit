48707ddeb4b69967f15a25a9e150c116d653c9d4
李康 <likangios@163.com>
2015-06-18 14:28:36 +0800

    hello github

---
+++ FirstGitDemo/FirstGitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstGitDemo
+//
+//  Created by 李康 on 15/6/18.
+//  Copyright (c) 2015年 Luck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstGitDemo/FirstGitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstGitDemo
+//
+//  Created by 李康 on 15/6/18.
+//  Copyright (c) 2015年 Luck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
